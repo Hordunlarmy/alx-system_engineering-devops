9abfb59cd9e6e63a61a4355960a3949c7591e8b2
hordunlarmy <hordunlarmy@gmail.com>
2023-12-05 07:26:58 +0000

    Add custom HTTP response header to Nginx configuration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,16 @@
 # Use Puppet to automate the task of creating a custom HTTP header response
 
-exec { 'update':
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
+package {'nginx':
   ensure => 'present',
-  require => Exec['update'],
 }
-
 file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-  require => Package['nginx'],
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  subscribe => File_line['http_header'],
+}
+exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
