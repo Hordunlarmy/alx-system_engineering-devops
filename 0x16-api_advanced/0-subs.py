0de6e1c9cd5f73fb93eff6adccd1b3ac6f3f61d6
hordunlarmy <hordunlarmy@gmail.com>
2024-01-16 21:58:19 +0000

    Refactor 0-subs.py to use string formatting

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     user_agent = {'User-agent': 'Google Chrome Version  120.0.6099.199'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
 
