6bb8dae496c8df2fdcc847efc2fed5ac2fd1b4ef
hordunlarmy <hordunlarmy@gmail.com>
2024-03-14 16:44:10 +0000

    Refactor files for readability

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.py
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.py
@@ -1,7 +1,6 @@
 # This manuscript increases the amount of traffic an Nginx server can handle
 
 # Increase ULIMIT of the default file
-
 exec { 'fix--for-nginx':
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
 /etc/default/nginx;\"",
@@ -9,7 +8,8 @@ exec { 'fix--for-nginx':
 }
 
 # Restart Nginx
--> exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
+
